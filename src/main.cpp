eb26c66466f2db0ad2747d89e78947810c881daa
Chun Shen <chunshen1987@gmail.com>
2014-11-07 15:01:22 -0500

    take out gsl dependence

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,6 @@
 #include<math.h>
 #include<sys/time.h>
 
-#include<gsl/gsl_sf_bessel.h>
-#include<gsl/gsl_rng.h>
-#include<gsl/gsl_randist.h>
-
 #include "Stopwatch.h"
 #include "parameters.h"
 #include "readindata.h"
